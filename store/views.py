3265fbe80f2e7b746804e85ef478d3708037b1a0
Joseph Ezekiel <theolujay@gmail.com>
2026-01-20 19:51:09 +0100

    feat: add homepage view

--- store/views.py
+++ store/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from store.models import Product
 
-# Create your views here.
+def home(request):
+    """Homepage with featured products"""
+
+    featured_products = Product.objects.filter(available=True)[:8]
+    context = {
+        "featured_products": featured_products,
+        "title": "Welcome to ShopHub"
+    }
+    return render(request, "store/home.html", context)
