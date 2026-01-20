c8e292286092e80950f2e34fef35253e61f95b27
Joseph Ezekiel <theolujay@gmail.com>
2026-01-20 19:28:26 +0100

    feat: improve admin interface for product and category

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category
 
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Category admin interface"""
+
+    list_display = ["name", "created_at"]
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Product admin interface"""
+
+    list_display = [
+        "name",
+        "category",
+        "price",
+        "stock",
+        "available",
+        "created_at",
+    ]
+    list_filter = ["available", "category", "created_at"]
+    search_fields = ["name", "description"]
