bf28d58faf743a30bcbe433130ccbe0dc09ae505
Joseph Ezekiel <theolujay@gmail.com>
2026-01-20 18:35:54 +0100

    feat: add product and category models

--- store/models.py
+++ store/models.py
@@ -15,6 +15,11 @@ class Product(models.Model):
 
     name = models.CharField(max_length=200)
     description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products',
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     available = models.BooleanField(default=True)
