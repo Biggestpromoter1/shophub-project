c54e6ea50aee611008a9fe64bdb8eb8cf0be2f87
Biggestpromoter1 <biggestpromoter1@gmail.com>
2026-01-27 02:57:14 +0100

    Added brand model,linked to product registered in admin

--- store/models.py
+++ store/models.py
@@ -1,42 +1,78 @@
 from django.db import models
+from django.utils.text import slugify
+
 
 class Category(models.Model):
     """Product categories"""
+
     name = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, null=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['name']
-        verbose_name_plural = 'Categories'
+        ordering = ["name"]
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=120, unique=True, blank=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     """Product model"""
 
     name = models.CharField(max_length=200)
     description = models.TextField()
-    slug = models.SlugField(max_length=200, unique=True, null=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
+
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
-        related_name='products',
-        null=True,
+        related_name="products",
     )
+
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE,
+        related_name="products",
+        null=True,
+        blank=True,
+    )
+
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     available = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to="products/", blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
 
     def __str__(self):
         return self.name
-    
-    # @property
+
+    @property
     def is_in_stock(self):
-        """Check if product is in stock"""
         return self.stock > 0
