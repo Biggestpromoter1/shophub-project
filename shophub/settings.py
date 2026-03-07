7cd7ab9a064bece2039a90bc6a8d6475d59cd1cb
Joseph Ezekiel <theolujay@gmail.com>
2026-03-07 08:29:54 +0100

    refactor: improve UI, add better search, add script

--- shophub/settings.py
+++ shophub/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%u!cm%sc6oto85fff$h1a-shwe)t5-iu-qd@7#xcine=9&p$bl'
+# TODO: use environment variables.
+SECRET_KEY = "django-insecure-%u!cm%sc6oto85fff$h1a-shwe)t5-iu-qd@7#xcine=9&p$bl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -32,52 +33,52 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'store',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "store",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'shophub.urls'
+ROOT_URLCONF = "shophub.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'shophub.wsgi.application'
+WSGI_APPLICATION = "shophub.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -87,16 +88,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -104,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+# TODO: use Africa/Lagos
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -116,6 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- store/views.py
+++ store/views.py
@@ -1,61 +1,84 @@
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+
 from .models import Product, Category
+
 
 def home(request):
     """Homepage with featured products"""
     featured_products = Product.objects.filter(
-        available=True
-    )[:8]  # Get 8 latest available products
-    
-    context = {
-        'featured_products': featured_products,
-        'title': 'Welcome to ShopHub'
-    }
-    return render(request, 'store/home.html', context)
+        available=True)[:8]  # Get 8 latest available products
+
+    context = {"featured_products": featured_products, "title": "Welcome to ShopHub"}
+    return render(request, "store/home.html", context)
 
 
 def product_list(request, category_slug=None):
-    """List all products or products by category"""
+    """List all products or products by category with pagination and sorting"""
     category = None
     products = Product.objects.filter(available=True)
-    
+    categories = Category.objects.annotate(product_count=Count("products"))
+
+    # Sorting
+    sort_by = request.GET.get("sort", "-created_at")
+    valid_sorts = ["price", "-price", "name", "-name", "-created_at"]
+    if sort_by in valid_sorts:
+        products = products.order_by(sort_by)
+
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    
+
+    # Pagination
+    # Task: research pagination, understand what it is and why it is sometimes used
+    paginator = Paginator(products, 6)  # 6 products per page
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'category': category,
-        'products': products,
-        'title': f'{category.name} Products' if category else 'All Products'
+        "category": category,
+        "products": page_obj,
+        "categories": categories,
+        "current_sort": sort_by,
+        "title": f"{category.name} Products" if category else "All Products",
     }
-    return render(request, 'store/product_list.html', context)
+    return render(request, "store/product_list.html", context)
 
 
 def product_detail(request, slug):
     """Product detail page"""
-    product = get_object_or_404(
-        Product, 
-        slug=slug, 
-        available=True
-    )
-    
+    product = get_object_or_404(Product, slug=slug, available=True)
+
     # Get related products from same category
     related_products = Product.objects.filter(
-        category=product.category,
-        available=True
+        category=product.category, available=True
     ).exclude(id=product.id)[:4]
-    
-    context = {
-        'product': product,
-        'related_products': related_products,
-        'title': product.name
-    }
-    return render(request, 'store/product_detail.html', context)
-def search(request):
-    query = request.GET.get('q')
-    products = Product.objects.filter(name__icontains=query)
 
-    return render(request, 'store/search.html', {
-        'products': products,
-        'query': query,
-    })
+    context = {
+        "product": product,
+        "related_products": related_products,
+        "title": product.name,
+    }
+    return render(request, "store/product_detail.html", context)
+
+
+# Added a search function to handle database queries
+def search(request):
+    # added a default (""), so the app gracefully handles when there's no query param
+    query = request.GET.get("q", "")
+    # better to name your variables well, so you have an idea what it returns or what it's for
+    products_list = Product.objects.filter(name__icontains=query, available=True)
+
+    paginator = Paginator(products_list, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "store/search.html",
+        {
+            "products": page_obj,
+            "query": query,
+        },
+    )
